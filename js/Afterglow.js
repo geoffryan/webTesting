22ec6aabba3410777a21af4b9f5ef60c88d8d680
Geoff Ryan <geoffrey.s.ryan@gmail.com>
2022-05-05 15:51:39 -0400

    more updares

--- js/Afterglow.js
+++ js/Afterglow.js
@@ -1,18 +1,32 @@
 
 class State {
-    constructor(t0, Fp0, num0, nuc0, p, nu_lc, t_sp) {
+    constructor(t0, Fp0, num0, nuc0, p, t_lc_min, t_lc_max, nu_lc,
+                nu_sp_min, nu_sp_max, t_sp, F_min, F_max) {
         this.t0 = t0;
         this.Fp0 = Fp0;
         this.num0 = num0;
         this.nuc0 = nuc0;
         this.p = p;
+
+        this.t_lc_min = t_lc_min;
+        this.t_lc_max = t_lc_max;
+        this.t_lc = geomspace(t_lc_min, t_lc_max, 300);
         this.nu_lc = nu_lc;
+        
+        this.nu_sp_min = nu_sp_min;
+        this.nu_sp_max = nu_sp_max;
+        this.nu_sp = geomspace(nu_sp_min, nu_sp_max, 300);
         this.t_sp = t_sp;
 
-        this.initialize();
+        this.F_min = F_min;
+        this.F_max = F_max;
+
+        this.traces = []
+
+        this.get_elements();
     }
 
-    initialize() {
+    get_elements() {
         this.plotDiv = document.getElementById("plot");
         this.t0_slider = document.getElementById("t0_slider");
         this.t0_text = document.getElementById("t0_text");
@@ -26,33 +40,77 @@ class State {
         this.p_text = document.getElementById("p_text");
     }
 
+    initialize_plot() {
+
+        this.traces = [];
+
+        let N_lc = this.nu_lc.length;
+        let N_sp = this.t_sp.length;
+
+        for ( let i = 0; i < N_lc; i++)
+        {
+            let Fnu = lightcurve(this.t_lc, this.nu_lc[i], this.t0, this.num0,
+                                this.nuc0, this.Fp0, this.p);
+            let name = "ν = " + this.nu_lc[i].toExponential(1) + " Hz";
+            this.traces.push({x: this.t_lc, y: Fnu,
+                              xaxis: 'x', yaxis: 'y',
+                              name: name});
+        }
+        for ( let i = 0; i < N_sp; i++)
+        {
+            let Fnu = spectrum(this.nu_sp, this.t_sp[i], this.t0, this.num0,
+                                this.nuc0, this.Fp0, this.p);
+            let name = "t = " + this.t_sp[i].toExponential(1) + " s";
+            this.traces.push({x: this.nu_sp, y: Fnu,
+                              xaxis: 'x2', yaxis: 'y2',
+                              name: name});
+        }
+    
+        this.layout = {
+            grid: {rows: 1, columns: 2, pattern: 'independent'},
+            xaxis: {type: 'log',
+                    range: [Math.log10(this.t_lc_min),
+                            Math.log10(this.t_lc_max)]},
+            yaxis: {type: 'log',
+                    range: [Math.log10(this.F_min),
+                            Math.log10(this.F_max)]},
+            xaxis2: {type: 'log',
+                    range: [Math.log10(this.nu_sp_min),
+                            Math.log10(this.nu_sp_max)]},
+            yaxis2: {type: 'log',
+                    range: [Math.log10(this.F_min),
+                            Math.log10(this.F_max)]},
+        };
+
+    }
+
+    recalc_traces() {
+        let N_lc = this.nu_lc.length;
+        let N_sp = this.t_sp.length;
+
+        for ( let i = 0; i < N_lc; i++)
+        {
+            this.traces[i].y = lightcurve(this.traces[i].x, this.nu_lc[i],
+                                this.t0, this.num0, this.nuc0, this.Fp0,
+                                this.p);
+        }
+        for ( let i = 0; i < N_sp; i++)
+        {
+            this.traces[N_lc+i].y = spectrum(this.traces[i].x, this.t_sp[i],
+                                this.t0, this.num0, this.nuc0, this.Fp0,
+                                this.p);
+        }
+    }
+
     render() {
 
-        let tr_lc1 = this.plotDiv.data[0]
-        let tr_lc2 = this.plotDiv.data[1]
-        let tr_lc3 = this.plotDiv.data[2]
-        let tr_sp1 = this.plotDiv.data[3]
-        let tr_sp2 = this.plotDiv.data[4]
+        console.log("render");
 
-        tr_lc1.y = lightcurve(tr_lc1.x, this.nu_lc[0], this.t0, this.num0,
-                                this.nuc0, this.Fp0, this.p);
-        tr_lc2.y = lightcurve(tr_lc2.x, this.nu_lc[1], this.t0, this.num0,
-                                this.nuc0, this.Fp0, this.p);
-        tr_lc3.y = lightcurve(tr_lc3.x, this.nu_lc[2], this.t0, this.num0,
-                                this.nuc0, this.Fp0, this.p);
-        tr_sp1.y = spectrum(tr_sp1.x, this.t_sp[0], this.t0, this.num0,
-                                this.nuc0, this.Fp0, this.p);
-        tr_sp2.y = spectrum(tr_sp2.x, this.t_sp[1], this.t0, this.num0,
-                                this.nuc0, this.Fp0, this.p);
-
-        console.log(this);
-        console.log(Math.min(...tr_lc1.y));
-        console.log(Math.max(...tr_lc1.y));
+        this.recalc_traces();
 
         this.update_inputs();
 
-        Plotly.react(this.plotDiv, [tr_lc1, tr_lc2, tr_lc3, tr_sp1, tr_sp2],
-                     plotDiv.layout);
+        Plotly.react(this.plotDiv, this.traces, this.layout);
     }
 
     update_inputs() {
@@ -137,8 +195,6 @@ function spectrum(nu, t_spec, t0, nu_m0, nu_c0, Fp, p) {
     return nu.map(nui => flux(t_spec, nui, t0, nu_m0, nu_c0, Fp, p));
 }
 
-let state = new State(3600.0, 1.0, 1.0e14, 1.0e17, 2.5,
-                      [1.0e10, 1.0e14, 1.0e18], [3600, 3.6e6]);
 
 const inputElement = document.getElementById('input');
 
@@ -154,64 +210,26 @@ async function handleFiles() {
 
 
 
-var t0 = 3600.0;
-var nu_m0 = 1.0e17;
-var nu_c0 = 1.0e16;
-var p = 2.5;
-var Fp0 = 1.0;
+const t0 = 3600.0;
+const Fp0 = 1.0;
+const num0 = 1.0e17;
+const nuc0 = 1.0e16;
+const p = 2.5;
 
-var nu1 = 1.0e10;
-var nu2 = 1.0e14;
-var nu3 = 1.0e18;
-var t1 = 3600.0;
-var t2 = 1e6;
+const nu_lc = [1.0e10, 1.0e14, 1.0e18];
+const t_sp = [3.6e3, 3.6e5];
 
-var tMin = 1e1;
-var tMax = 1e8;
-var nuMin = 1e6;
-var nuMax = 1e20;
+const tMin = 1e1;
+const tMax = 1e8;
+const nuMin = 1e6;
+const nuMax = 1e20;
+const Fmin = 1e-7;
+const Fmax = 1e1;
 
-var Nt = 300;
-var Nnu = 300;
-
-var t = geomspace(tMin, tMax, Nt);
-var nu = geomspace(nuMin, nuMax, Nnu);
-
-var F_lc_1 = lightcurve(t, nu1, t0, nu_m0, nu_c0, Fp0, p);
-var F_lc_2 = lightcurve(t, nu2, t0, nu_m0, nu_c0, Fp0, p);
-var F_lc_3 = lightcurve(t, nu3, t0, nu_m0, nu_c0, Fp0, p);
-
-var F_sp_1 = spectrum(nu, t1, t0, nu_m0, nu_c0, Fp0, p);
-var F_sp_2 = spectrum(nu, t2, t0, nu_m0, nu_c0, Fp0, p);
-
-var tr_lc1 = {x: t, y: F_lc_1, xaxis: 'x', yaxis: 'y'};
-var tr_lc2 = {x: t, y: F_lc_2, xaxis: 'x', yaxis: 'y'};
-var tr_lc3 = {x: t, y: F_lc_3, xaxis: 'x', yaxis: 'y'};
-
-var tr_sp1 = {x: nu, y: F_sp_1, xaxis: 'x2', yaxis: 'y2'};
-var tr_sp2 = {x: nu, y: F_sp_2, xaxis: 'x2', yaxis: 'y2'};
-
-var minLogT = Math.log10(tMin);
-var maxLogT = Math.log10(tMax);
-var minLogNu = Math.log10(nuMin);
-var maxLogNu = Math.log10(nuMax);
-
-var minLogF = Math.log10(1e-7 * Fp0);
-var maxLogF = Math.log10(1e1 * Fp0);
-
-
-var layout = {
-    grid: {rows: 1, columns: 2, pattern: 'independent'},
-    xaxis: {type: 'log', range: [minLogT, maxLogT]},
-    yaxis: {type: 'log', range: [minLogF, maxLogF]},
-    xaxis2: {type: 'log', range: [minLogNu, maxLogNu]},
-    yaxis2: {type: 'log', range: [minLogF, maxLogF]},
-};
-
-
-let plotDiv = document.getElementById("plot");
-Plotly.newPlot(plotDiv,
-    [tr_lc1, tr_lc2, tr_lc3, tr_sp1, tr_sp2], layout);
+let state = new State(t0, Fp0, num0, nuc0, p, tMin, tMax, nu_lc,
+                      nuMin, nuMax, t_sp, Fmin, Fmax);
+state.initialize_plot();
+state.render();
 
 let t0_slider = document.getElementById("t0_slider");
 let t0_text = document.getElementById("t0_text");
@@ -227,70 +245,70 @@ let p_text = document.getElementById("p_text");
 t0_slider.oninput = function() {
     let val = Math.pow(10.0, this.valueAsNumber);
     state.t0 = val;
-    state.render(plotDiv);
+    state.render();
 };
 
 t0_text.onchange = function() {
     let val = Number(this.value);
     if (!isNaN(val)) {
         state.t0 = val;
-        state.render(plotDiv);
+        state.render();
     }
 };
 
 Fp0_slider.oninput = function() {
     let val = Math.pow(10.0, this.valueAsNumber);
     state.Fp0 = val;
-    state.render(plotDiv);
+    state.render();
 };
 
 Fp0_text.onchange = function() {
     let val = Number(this.value);
     if (!isNaN(val)) {
         state.Fp0 = val;
-        state.render(plotDiv);
+        state.render();
     }
 };
 
 num0_slider.oninput = function() {
     let val = Math.pow(10.0, this.valueAsNumber);
     state.num0 = val;
-    state.render(plotDiv);
+    state.render();
 };
 
 num0_text.onchange = function() {
     let val = Number(this.value);
     if (!isNaN(val)) {
         state.num0 = val;
-        state.render(plotDiv);
+        state.render();
     }
 };
 
 nuc0_slider.oninput = function() {
     let val = Math.pow(10.0, this.valueAsNumber);
     state.nuc0 = val;
-    state.render(plotDiv);
+    state.render();
 };
 
 nuc0_text.onchange = function() {
     let val = Number(this.value);
     if (!isNaN(val)) {
         state.nuc0 = val;
-        state.render(plotDiv);
+        state.render();
     }
 };
 
 p_slider.oninput = function() {
     let val = this.valueAsNumber;
     state.p = val;
-    state.render(plotDiv);
+    state.render();
 };
 
 p_text.onchange = function() {
     let val = Number(this.value);
     if (!isNaN(val)) {
         state.p = val;
-        state.render(plotDiv);
+        state.render();
     }
 };
 
